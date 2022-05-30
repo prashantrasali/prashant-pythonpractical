77f1256e03047679c7c9ba9bfed1af3a2272f25b
prashantrasali <106474538+prashantrasali@users.noreply.github.com>
2022-05-30 19:48:43 +0530

    Add files via upload

---
+++ numpygithub/numpy2.py
@@ -0,0 +1,12 @@
+import numpy as np 
+n=int(input()) #no. of rows
+m=int(input()) #no. of columns
+s=[]
+for i in range(n):
+    l=list(map(int,input().split()))
+    for i in l:
+        s.append(i)
+a=np.array(s)
+a.shape=(n,m)
+print(a)
+© 2022 GitHub, Inc.
